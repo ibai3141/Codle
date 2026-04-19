cacb49fa182b88be5f30ce9ca8aff3e73588f72c
ibai3141 <ibaitituanabuenano@gmail.com>
2026-04-19 13:49:42 +0200

    endpoint para crear partida

--- backend/api/clasico/clasico.py
+++ backend/api/clasico/clasico.py
@@ -14,6 +14,7 @@ from fastapi import APIRouter, HTTPException, Header
 from api.utils.keys import SUPABASE_URL, SUPABASE_KEY
 from supabase import create_client
 from api.utils import tokens
+import random
 
 router = APIRouter(prefix="/clasico", tags=["clasico"])
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
@@ -62,7 +63,20 @@ El backend debe:
 - `puntuacion = 0`
 - `iniciada_en`
 ```'''
+def buscar_lenguaje():
+    try:
+        resul = supabase.table("lenguaje").select("id").eq("activo", True).execute()
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error al buscar un lenguaje objetivo: {str(e)}")
 
+    if not resul.data:
+        raise HTTPException(status_code=404, detail="No hay lenguajes activos disponibles para crear la partida")
+
+    lenguaje_elegido = random.choice(resul.data)
+    return lenguaje_elegido["id"]
+
+
+    
 @router.post("/crear_partida")
 def crear_partida(Authorization: str = Header(...)):
     
@@ -71,27 +85,34 @@ def crear_partida(Authorization: str = Header(...)):
 
     token = Authorization.replace("Bearer ","")
     info = tokens.decodificar_token_acceso(token)
-    id_usuario = info["sub"]
+    id_usuario = int(info["sub"])
 
-    result = supabase.table("partida").insert({
-        "usuario_id":id_usuario,
-        "modo": "CLASICO",
-        "lenguaje_objetivo_id":1,
-        "estado":"en_curso",
-        "fase_actual":"lenguaje",
-        "max_intentos":null,
-        "intentos_usados":0,
-        "puntuacion":0,
-        "iniciada_en":0
-    }).execute()
+    lenguaje_id = buscar_lenguaje()
 
-    if result:
-        return {"partida_id": 12,
-                "modo": result["modo"],
-                "estado": result["estado"]
-                }
+    try:
+        result = supabase.table("partida").insert({
+            "usuario_id": id_usuario,
+            "modo": "CLASICO",
+            "lenguaje_objetivo_id": lenguaje_id,
+            "estado": "en_curso",
+            "fase_actual": "lenguaje",
+            "max_intentos": None,
+            "intentos_usados": 0,
+            "puntuacion": 0
+        }).execute()
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error al crear la partida: {str(e)}")
+
+    if result.data:
+        partida = result.data[0]
+
+        return {
+            "partida_id": partida["id"],
+            "modo": partida["modo"],
+            "estado": partida["estado"]
+            }
     else:
-        raise Exception
+        raise HTTPException(status_code=500, detail="No se pudo crear la partida")
         
 
     
